33b2251ec1d8a928d686cf1e8520434b4d08ff1e
Chillyssa <38333607+chillyssa@users.noreply.github.com>
2022-04-18 23:06:15 -0600

    Adding the code to Student.java to list all Students with their interests

--- java/src/main/java/Student.java
+++ java/src/main/java/Student.java
@@ -1,7 +1,7 @@
 /*
  * CS3810 - Principles of Database Systems - Spring 2022
  * Instructor: Thyago Mota
- * Student Names:
+ * Student Names: Alyssa Williams and Karent Correa 
  * Description: creates a Student entity with a 1-many mapping to StudentInterest and allows listing of all students
  */
 
@@ -10,17 +10,97 @@ import java.io.Serializable;
 import java.util.List;
 
 // TODO: finish the object-relational mapping
+
 class StudentInterestPK implements Serializable {
+    private String stEmail, intAbbrv; 
 
+    public String getStEmail(){
+        return stEmail;
+    }
+
+    public void setStEmail(String stEmail){
+        this.stEmail = stEmail; 
+    }
+
+    public String getIntAbbrv(){
+        return intAbbrv;
+    }
+
+    public void setIntAbbrv(String intAbbrv){
+        this.intAbbrv = intAbbrv;
+    }
+
+    @Override
+    public String toString(){
+        return "StudentInterestPK{stEmail = " + stEmail + ", intAbbrv = " + intAbbrv + "}";
+    }
 }
 
 // TODO: finish the object-relational mapping
+@Entity
+@Table(name = "StudentInterests")
 class StudentInterest implements Serializable {
+    @EmbeddedId
+    private StudentInterestPK stIntPK; 
 
+    public StudentInterestPK getStudentInterestPK() {
+        return stIntPK;
+    }
+
+    public void setStudentInterestPK(StudentInterestPK stIntPK) {
+        this.stIntPK = stIntPK;
+    }
+
+    @Override
+    public String toString(){
+        return "StudentInterests{ StudentInterestPK = " + stIntPK +" }";
+    }
 }
 
 // TODO: finish the object-relational mapping
+@Entity 
+@Table(name = "Students")
 class Student {
+    @Id
+    private String email; 
+    
+    private String name, major, graduation; 
+
+    public String getEmail(){
+        return email; 
+    }
+    public void setEmail(String email){
+        this.email = email; 
+    }
+
+    public String getName(){
+        return name; 
+    }
+
+    public void setName(String name){
+        this.name = name; 
+    }
+
+    public String getMajor(){
+        return major; 
+    }
+
+    public void setMajor(String major){
+        this.major = major; 
+    }
+
+    public String getGraduation(){
+        return graduation; 
+    }
+
+     public void setGraduation(String graduation){
+        this.graduation = graduation; 
+    }
+
+   @Override
+   public String toString(){
+       return "Students{ email = " + email + ", name = " + name + ", major = " + major + ", graduation = " + graduation + " }";
+   }
 
     public static void main(String[] args) {
         
@@ -29,6 +109,14 @@ class Student {
         EntityManager em = emf.createEntityManager();
 
         // TODO: list all students
+
+        // Create query string that includes a join for the Students and StudentInterests table to display students with each interest
+        Query query = em.createQuery("SELECT a FROM Student a");
+        //List<String> students = new List<String> ();
+        for(Object obj:  query.getResultList()){
+            Student student = (Student) obj; 
+            System.out.println(student);
+        }
         
         em.close();
     }
